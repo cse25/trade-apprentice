9f82870d51b87710e779f394ad1db1890c4e015f
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-04-04 15:52:57 -0700

    able to buy stocks

--- client/app.js
+++ client/app.js
@@ -3,48 +3,54 @@ var app = angular.module('myApp', []);
 
 app.controller('FormController', function($scope, $http) {
 	$scope.retrievedData = {
+		name: null,
 		symbol: null,
-		price: null
+		price: null,
+		timestamp: null,
+		open: null,
+		high: null,
+		low: null
 	};
+
+	$scope.state = false;
+
+	$scope.portfolio = []
 
 	$scope.stocksymbol = {
 		symbol: ''
 	};
 
+	$scope.numShares;
+
 	$scope.inputStockSymbol = function() {
-		console.log('inside inputStockSymbol', $scope.stocksymbol)
-		// $scope.retrievedData.price = '$100';
-		// $scope.retrievedData.symbol = 'APL';
 		$http({
 		  method: 'GET',
 		  url: 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=' + $scope.stocksymbol.symbol + '&callback=myFunction'
 		}).then(function successCallback(response) {
-			console.log('response.data :', response.data);
 			var string = response.data;
 			var newString = string.slice(11,-1);
-			console.log('newString: ', newString);
 			var dataObject = JSON.parse(newString);
-			console.log('parsedString: ', dataObject);
-			console.log('dataObject.Name: ', dataObject.Name);
-			console.log('dataObject.LastPrice: ', dataObject.LastPrice);
-			$scope.retrievedData.symbol = dataObject.Name;
+			$scope.retrievedData.name = dataObject.Name;
+			$scope.retrievedData.symbol = dataObject.Symbol
 			$scope.retrievedData.price = dataObject.LastPrice;
-			// console.log('retrievedData.symbol: ', retrievedData.symbol);
-			// console.log('retrievedData.price: ', retrievedData.price);
-		    
-		    // this callback will be called asynchronously
-		    // when the response is available
+			$scope.retrievedData.timestamp = dataObject.Timestamp;
+			$scope.retrievedData.open = dataObject.Open;
+			$scope.retrievedData.high = dataObject.High;
+			$scope.retrievedData.low = dataObject.Low;
+			$scope.state = true;
 		  }, function errorCallback(response) {
-		    // called asynchronously if an error occurs
-		    // or server returns response with an error status.
+		  	console.log('Error');
 		  });
-		//http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=GOOG&callback=myFunction
-		//when I get info back from get request:
-		//display information
-		//make get request
-		//with promise/callback
-		//when data is sent back
-		//upon no error
-		//assign to retrievedData object
+	};
+
+	$scope.buyShares = function() {
+		console.log('Buying ' + $scope.numShares + ' Shares of ' + $scope.retrievedData.symbol + ' for $' + ($scope.numShares * $scope.retrievedData.price));
+		var purchase = {
+			shares: $scope.numShares,
+			symbol: $scope.retrievedData.symbol,
+			total: $scope.numShares * $scope.retrievedData.price
+		};
+		$scope.portfolio.push(purchase);
+		console.log($scope.portfolio);
 	};
 });
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-	// res.send('post');
+
 });
 
 app.listen(3000, function () {
