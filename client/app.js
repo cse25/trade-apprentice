1c72160b3407dcd7340b3a9d85a1e740e961e6c6
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-04-04 13:50:42 -0700

    basic input form complete

---
+++ client/app.js
@@ -0,0 +1,38 @@
+//create Angular module named 'myApp'
+var app = angular.module('myApp', []);
+
+app.controller('FormController', function($scope, $http) {
+	$scope.retrievedData = {
+		symbol: null,
+		price: null
+	}
+
+	$scope.stocksymbol = {
+		symbol: ''
+	};
+
+	$scope.inputStockSymbol = function() {
+		console.log('inside inputStockSymbol', $scope.stocksymbol)
+		// $scope.retrievedData.price = '$100';
+		// $scope.retrievedData.symbol = 'APL';
+		$http({
+		  method: 'GET',
+		  url: 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=' + $scope.stocksymbol + '&callback=myFunction'
+		}).then(function successCallback(response) {
+			console.log('response', response);
+		    // this callback will be called asynchronously
+		    // when the response is available
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+		//http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=GOOG&callback=myFunction
+		//when I get info back from get request:
+		//display information
+		//make get request
+		//with promise/callback
+		//when data is sent back
+		//upon no error
+		//assign to retrievedData object
+	};
+});
---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(__dirname + '/../client'));
+
+app.get('/', function (req, res) {
+  	res.render('index');
+});
+
+app.post('/', function (req, res) {
+	// res.send('post');
+});
+
+app.listen(3000, function () {
+  	console.log('listening on port 3000');
+});
