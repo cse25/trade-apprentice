0b538aae86a2d4074cfe4a6170762ab7fc4bd1ba
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-04-04 19:26:26 -0700

    working on portfolio update function

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
 
 app.controller('FormController', function($scope, $http) {
-	$scope.retrievedData = {
+	$scope.stockData = {
 		name: null,
 		symbol: null,
 		price: null,
@@ -17,7 +17,7 @@ app.controller('FormController', function($scope, $http) {
 
 	$scope.portfolioTotalValue = 0;
 
-	$scope.getPortfolioTotalValue = function() {
+	$scope.updatePortfolioTotalValue = function() {
 		$scope.portfolioTotalValue = $scope.portfolio.map(function(item) {
 			return item.total;
 		}).reduce(function(a,b) {
@@ -37,15 +37,11 @@ app.controller('FormController', function($scope, $http) {
 		$scope.portfolioSnapshot.push($scope.portfolioTotalValue);
 	};
 
-	$scope.checkPortfolioPerformance = function() {
-		if ($scope.portfolioSnapshot.length < 2) {
-			console.log("0% Gains");
-		} else {
-			var mostRecentSnapshot = $scope.portfolioSnapshot[$scope.portfolioSnapshot.length -1];
-		}
+	$scope.viewHistoricPortfolioPerformance = function() {
+		//use portfolioSnapshot to make a graph of value
 	};
 
-	$scope.getStockPrice = function() {
+	$scope.getStockData = function() {
 		$http({
 		  method: 'GET',
 		  url: 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=' + $scope.stocksymbol.symbol + '&callback=myFunction'
@@ -53,13 +49,13 @@ app.controller('FormController', function($scope, $http) {
 			var string = response.data;
 			var newString = string.slice(11,-1);
 			var dataObject = JSON.parse(newString);
-			$scope.retrievedData.name = dataObject.Name;
-			$scope.retrievedData.symbol = dataObject.Symbol
-			$scope.retrievedData.price = dataObject.LastPrice;
-			$scope.retrievedData.timestamp = dataObject.Timestamp;
-			$scope.retrievedData.open = dataObject.Open;
-			$scope.retrievedData.high = dataObject.High;
-			$scope.retrievedData.low = dataObject.Low;
+			$scope.stockData.name = dataObject.Name;
+			$scope.stockData.symbol = dataObject.Symbol
+			$scope.stockData.price = dataObject.LastPrice;
+			$scope.stockData.timestamp = dataObject.Timestamp;
+			$scope.stockData.open = dataObject.Open;
+			$scope.stockData.high = dataObject.High;
+			$scope.stockData.low = dataObject.Low;
 			$scope.searchTermExists = true;
 		  }, function errorCallback(response) {
 		  	console.log('Error');
@@ -67,17 +63,26 @@ app.controller('FormController', function($scope, $http) {
 	};
 
 	$scope.buyShares = function() {
-		console.log('Buying ' + $scope.numShares + ' Shares of ' + $scope.retrievedData.symbol + ' for $' + ($scope.numShares * $scope.retrievedData.price));
+		console.log('Buying ' + $scope.numShares + ' Shares of ' + $scope.stockData.symbol + ' for $' + ($scope.numShares * $scope.stockData.price));
 		var purchase = {
 			shares: $scope.numShares,
-			symbol: $scope.retrievedData.symbol,
-			sharePrice: $scope.retrievedData.price,
-			total: $scope.numShares * $scope.retrievedData.price
+			symbol: $scope.stockData.symbol,
+			sharePrice: $scope.stockData.price,
+			total: $scope.numShares * $scope.stockData.price
 		};
 		$scope.portfolio.push(purchase);
 		console.log($scope.portfolio);
-		$scope.getPortfolioTotalValue();
+		$scope.updatePortfolioTotalValue();
 		$scope.takeSnapshot();
 		console.log($scope.portfolioSnapshot);
 	};
+
+	$scope.updatePortfolioToMarketValue = function() {
+		console.log('Inside updatePortfolioToMarketValue');
+		//for each item in portfolio...
+		$scope.portfolio = $scope.portfolio.forEach(function(item){
+			console.log(item);
+			$scope.getStockData();
+		})
+	}
 });
